dd78b4138fc2b54c0bb227517e49f841548594bd
JaeHo Jeong <sean11301997@gmail.com>
2024-11-19 18:21:30 +0900

    Update 5-1
    AI

---
+++ TestUnrealEngine/Source/TestUnrealEngine/MyAIController.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTUNREALENGINE_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AMyAIController();
+
+	// ºùÀÇ
+	virtual void OnPossess(APawn* InPawn) override;
+	// ºùÀÇ ³¡
+	virtual void OnUnPossess() override;
+
+private:
+	void RandomMove();
+
+private:
+	FTimerHandle TimerHandle;
+	
+};
